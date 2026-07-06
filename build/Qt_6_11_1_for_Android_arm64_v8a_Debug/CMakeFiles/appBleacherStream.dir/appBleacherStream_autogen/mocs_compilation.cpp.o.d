06e9ee4b227e39df2b5cc28819b350f9b48187c7
haciM327 <micahdschwab@gmail.com>
2026-07-06 14:03:06 -0400

    Added default QT application

--- build/Qt_6_11_1_for_Android_arm64_v8a_Debug/CMakeFiles/appBleacherStream.dir/appBleacherStream_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/appBleacherStream.dir/appBleacherStream_autogen/mocs_compilation.cpp.o: \
-  /home/micah/BleacherStream/build/Qt_6_11_1_for_Android_arm64_v8a_Debug/appBleacherStream_autogen/mocs_compilation.cpp
